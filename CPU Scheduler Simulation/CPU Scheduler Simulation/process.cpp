03a0010b33200c4d0933f479ddb29553c7cbd664
geoffrey1218 <geoff.s.rich@gmail.com>
2017-03-16 18:20:56 -0700

    Implement Process class

--- CPU Scheduler Simulation/CPU Scheduler Simulation/process.cpp
+++ CPU Scheduler Simulation/CPU Scheduler Simulation/process.cpp
@@ -1,4 +1,5 @@
 #include "process.h"
+#include <iostream>
 
 using namespace std;
 
@@ -9,20 +10,71 @@ Process::Process(int id, int arrival_time, vector<int> bursts) : bursts(bursts) 
 	burst_index = 0;
 	cpu_wait = 0;
 	io_wait = 0;
+	last_in_cpu = -1;
+	last_in_io = -1;
+	is_finished = false;
 }
 
-int Process::cpu() {
+int Process::cpu(int current_time) {
+	if (!isCpuBurst()) {
+		cout << "Tried to call cpu when it was io\n";
+		cout << "Calling io instead...\n";
+		return io(current_time);
+	}
 
+	last_in_cpu = current_time;
+
+	return executeBurst();
 }
 
-int Process::io() {
+int Process::io(int current_time) {
+	if (!isIoBurst()) {
+		cout << "Tried to call io when it was cpu\n";
+		cout << "Calling cpu instead...\n";
+		return cpu(current_time);
+	}
 
+	last_in_io = current_time;
+
+	return executeBurst();
+}
+
+int Process::executeBurst() {
+	int new_value = --bursts[burst_index];
+
+	if (bursts[burst_index] <= 0) {
+		++burst_index;
+
+		if (burst_index >= bursts.size()) {
+			is_finished = true;
+		}
+	}
+
+	return new_value;
 }
 
 void Process::updateCpuWaitTime(int current_time) {
-
+	if (last_in_cpu == -1) {
+		cpu_wait = current_time;
+	} 
+	else {
+		cpu_wait += (current_time - last_in_cpu);
+	}	
 }
 
 void Process::updateIoWaitTime(int current_time) {
+	if (last_in_io == -1) {
+		io_wait = current_time;
+	}
+	else {
+		io_wait += (current_time - last_in_io);
+	}
+}
 
+bool Process::isCpuBurst() {
+	return (burst_index % 2 == 0);
+}
+
+bool Process::isIoBurst() {
+	return !isCpuBurst();
 }
--- CPU Scheduler Simulation/CPU Scheduler Simulation/process.h
+++ CPU Scheduler Simulation/CPU Scheduler Simulation/process.h
@@ -8,10 +8,15 @@ private:
 	int id;
 	vector<int> bursts;
 	int burst_index;
+
 	int cpu_wait;
 	int io_wait;
+	int last_in_cpu;
+	int last_in_io;
+
 	int arrival_time;
 	int exit_time;
+	bool is_finished;
 
 public:
 	Process(int id, int arrival_time, vector<int> bursts);
@@ -20,10 +25,16 @@ public:
 	int getArrivalTime() { return arrival_time; }
 	int getCpuWait() { return cpu_wait; }
 	int getIoWait() { return io_wait; }
+	bool isFinished() { return is_finished; }
 	
 
-	int cpu();
-	int io();
+	int cpu(int current_time);
+	int io(int current_time);
+	int executeBurst();
+
 	void updateCpuWaitTime(int current_time);
 	void updateIoWaitTime(int current_time);
+
+	bool isCpuBurst();
+	bool isIoBurst();
 };
