9b4243d6a63e9a7b0b793e1c1047d1bce32f5c8f
eparslow18 <eparslow18@LAB-SCI-304-24>
2017-03-16 18:07:07 -0700

    scheduler class created

---
+++ CPU Scheduler Simulation/CPU Scheduler Simulation/scheduler.cpp
@@ -0,0 +1,18 @@
+#include "scheduler.h"
+using namespace std;
+
+
+Scheduler::Scheduler() {
+
+}
+
+Process* Scheduler::schedule() {
+
+}
+
+void Scheduler::addProcess(Process* p) {
+
+}
+
+
+
---
+++ CPU Scheduler Simulation/CPU Scheduler Simulation/scheduler.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "process.h"
+using namespace std;
+
+class Scheduler {
+private:
+	Process* current_process;
+
+public:
+	//constructor 
+	Scheduler();
+
+	//getters
+	//Process* getCurrentProcess() { return Process* p;}
+
+	//setters 
+	void setCurrentProcess() { this->current_process = current_process; }
+
+	//
+	virtual Process* schedule();
+	void addProcess(Process* p);
+};
