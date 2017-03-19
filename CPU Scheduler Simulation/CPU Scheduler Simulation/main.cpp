52a907c0682f74bc60dd64d373e2ef7781158337
eparslow18 <eparslow18@LAB-SCI-304-24>
2017-03-19 15:33:18 -0700

    implementing fcfs

--- CPU Scheduler Simulation/CPU Scheduler Simulation/main.cpp
+++ CPU Scheduler Simulation/CPU Scheduler Simulation/main.cpp
@@ -3,10 +3,17 @@
 using namespace std;
 
 int main() {
-	OperatingSystem os = OperatingSystem(SchedulerType::SMALLEST_PROCESS_NEXT);
+	OperatingSystem os = OperatingSystem(SchedulerType::FIRST_COME_FIRST_SERVE);
 	os.generateProcessFile("process_list.txt", 12);
 	os.readProcessesFromFile("process_list.txt");
 	os.runProcesses();
+
+	cout << endl << "//////////////////////////////////////////////" << endl << "//////////////////////////////////////////////" <<endl <<endl;
+	os = OperatingSystem(SchedulerType::SMALLEST_PROCESS_NEXT);
+	os.generateProcessFile("process_list.txt", 12);
+	os.readProcessesFromFile("process_list.txt");
+	os.runProcesses();
+
 
 	return 0;
 }
