ddcf349a378d1ed17a21953560be07d9fe9dbbf3
geoffrey1218 <geoff.s.rich@gmail.com>
2017-03-18 18:14:07 -0700

    Update Scheduler documentation

--- CPU Scheduler Simulation/CPU Scheduler Simulation/scheduler.h
+++ CPU Scheduler Simulation/CPU Scheduler Simulation/scheduler.h
@@ -16,8 +16,9 @@ public:
 	//setters 
 	void setCurrentProcess() { this->current_process = current_process; }
 
-	//
+	/* @return the process that should be currently running on the CPU */
 	virtual Process* schedule() = 0;
+	/* Add a process to the scheduler's ready queue */
 	virtual void addProcess(Process* p) = 0;
 	/* @return the number of processes in the scheduler's ready queue */
 	virtual int getNumInReadyQueue() = 0;
