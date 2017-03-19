8780ce51aa0693e6b4130e24ec9042218e8994c0
eparslow18 <eparslow18@LAB-SCI-304-24>
2017-03-19 15:59:45 -0700

    testing fcfs and spn

--- CPU Scheduler Simulation/CPU Scheduler Simulation/main.cpp
+++ CPU Scheduler Simulation/CPU Scheduler Simulation/main.cpp
@@ -3,16 +3,15 @@
 using namespace std;
 
 int main() {
-	OperatingSystem os = OperatingSystem(SchedulerType::FIRST_COME_FIRST_SERVE);
+	OperatingSystem os = OperatingSystem(SchedulerType::ROUND_ROBIN);
 	os.generateProcessFile("process_list.txt", 12);
 	os.readProcessesFromFile("process_list.txt");
 	os.runProcesses();
 
-	cout << endl << "//////////////////////////////////////////////" << endl << "//////////////////////////////////////////////" <<endl <<endl;
-	os = OperatingSystem(SchedulerType::SMALLEST_PROCESS_NEXT);
-	os.generateProcessFile("process_list.txt", 12);
-	os.readProcessesFromFile("process_list.txt");
-	os.runProcesses();
+	//cout << endl << "///////////////////////////////////////////////////////////////////" << endl << "///////////////////////////////////////////////////////////////////" <<endl <<endl;
+	//os = OperatingSystem(SchedulerType::SMALLEST_PROCESS_NEXT);
+	//os.readProcessesFromFile("process_list.txt");
+	//os.runProcesses();
 
 
 	return 0;
