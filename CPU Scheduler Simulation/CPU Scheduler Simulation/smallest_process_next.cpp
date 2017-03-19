0dd81722f1d0da507405fe096b6d163f2539b137
geoffrey1218 <geoff.s.rich@gmail.com>
2017-03-18 17:50:45 -0700

    Implement SPN class
    - implement schedule and addProcess
    - sort priority queue by running time
    - remove getters and setters for priority queue

--- CPU Scheduler Simulation/CPU Scheduler Simulation/smallest_process_next.cpp
+++ CPU Scheduler Simulation/CPU Scheduler Simulation/smallest_process_next.cpp
@@ -6,16 +6,30 @@
 
 using namespace std;
 
-SmallestProcessNext::SmallestProcessNext() : Scheduler() {
-
+SmallestProcessNext::SmallestProcessNext() : Scheduler(), q() {
+	
 }
 
 
 Process* SmallestProcessNext::schedule() {
 	cout << "Scheduling SPN..." << endl;
-	return nullptr;
+	//If current process is still running on the CPU, let it keep running
+	if (current_process && current_process->isCpuBurst()) {
+		return current_process;
+	}
+	//Otherwise, if we have more processes in the queue...
+	else if (!q.empty()) {
+		current_process = q.top();
+		q.pop();
+		return current_process;
+	}
+	//Otherwise, return set current_process to nullptr - no processes left to run (for now)
+	else {
+		current_process = nullptr;
+		return current_process;
+	}
 }
 
 void SmallestProcessNext::addProcess(Process* p) {
-
+	q.push(p);
 }
--- CPU Scheduler Simulation/CPU Scheduler Simulation/smallest_process_next.h
+++ CPU Scheduler Simulation/CPU Scheduler Simulation/smallest_process_next.h
@@ -3,26 +3,30 @@
 #include <vector>
 #include <queue>
 
+/** Comparator used to sort the SPN priority queue */
+struct SPNComparator {
+	bool operator()(Process* const lhs, Process* const rhs) const{
+		return (lhs->getCurrentBurstLength() > rhs->getCurrentBurstLength());
+	}
+};
+
 class SmallestProcessNext : public Scheduler {
 private:
-	priority_queue<Process*> q;
+	/** Sorted on running time of current CPU burst */
+	priority_queue<Process*, vector<Process*>, SPNComparator> q;
 
 public:
 	//no arg construtor 
 	SmallestProcessNext();
 
-	//getters
-	priority_queue<Process*> getPriorityQueue() {
-		return q;
-	}
-
-	//setters
-	void setPriortyQueue(priority_queue<Process*> q) {
-		this->q = q;
-	}
-
-	//other functions
+	/**
+	Returns a process to run on the CPU using an SPN algorithm
+	Process is selected from the priority queue, sorted on running time of current CPU burst
+	@return process to run, or nullptr if none are in the queue
+	**/
 	Process* schedule();
+
+	/** Adds a process to the priority queue */
 	void addProcess(Process* p);
 
 
